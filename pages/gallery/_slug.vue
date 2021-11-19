94bcf150fd8abeb66a5667c8dca30f1983872bd4
lucas-cq <lucas.cummings024@gmail.com>
2021-11-19 16:56:04 -0700

    Added gallery and slug

---
+++ pages/gallery/_slug.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+  <GlobalNav />
+    <section>
+      <h2>{{ post.title }}</h2>
+      <nuxt-img :src="post.image" />
+      <p>Date Added: {{ post.date }}</p>
+    </section>
+  <MyFooter />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    try {
+      const post = await $content(`gallery/${params.slug}`).fetch()
+      return {
+        post,
+      }
+    } catch (error) {
+      error('Gallery Not found')
+    }
+  }
+}
+</script>
